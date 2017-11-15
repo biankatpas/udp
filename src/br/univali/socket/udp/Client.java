b5ef279cc3840b876c1a56fa702d68e15efc523f
Bianka Passos <biankatpas@gmail.com>
2017-11-15 19:57:53 -0200

    separando client em metodos

--- src/br/univali/socket/udp/Client.java
+++ src/br/univali/socket/udp/Client.java
@@ -6,8 +6,6 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -25,14 +23,15 @@ public class Client
 
     public Client(int port, String server, String name) 
     {
-        try 
+        try
         {
             this.port = port;
             this.name = name;
             this.server = server;
-            
+
             socket = new DatagramSocket();
             address = InetAddress.getByName(this.server);
+            
         } 
         catch (SocketException ex) 
         {
@@ -44,60 +43,42 @@ public class Client
         }
     }
 
-    public void run() 
+    public byte[] readMessage() 
     {
-        while (true) 
-        {
-            try 
-            {
-                //envio de dados para o emissor do datagrama recebido
-                String message = JOptionPane.showInputDialog(null, "Digite a mensagem do cliente "+name);
-                byte[] data = message.getBytes();
-                DatagramPacket sendBuffer = new DatagramPacket(data, data.length, address, port);
-                socket.send(sendBuffer);//envio
+        String message = JOptionPane.showInputDialog(null, "Digite a mensagem do cliente " + name);
+        return message.getBytes();
+    }
+    
+    public void sendMessage(byte[] data) throws IOException
+    {
+        DatagramPacket sendBuffer = new DatagramPacket(data, data.length, address, port);
+        socket.send(sendBuffer); //envio
+    }
+    
+    public DatagramPacket receiveMessage() throws IOException
+    {
+         DatagramPacket receiveBuffer = new DatagramPacket(new byte[1024], 1024, address, port);
+         socket.receive(receiveBuffer); //recepção
+         return receiveBuffer;
+    }
+    
+    public void printMessage(DatagramPacket receiveBuffer)
+    {
+        String receiveMessage = new String(receiveBuffer.getData());
+        System.out.println("O servidor disse: " + receiveMessage);
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                byte[] data = readMessage(); //solicita a msg a ser enviada
+                sendMessage(data); //envio dos dados para o emissor do datagrama recebido
+                DatagramPacket receiveBuffer = receiveMessage(); //recebimento dos dados em um buffer de 1024 bytes
+                printMessage(receiveBuffer); //imprime a msg recebida
                 
-                //recebimento dos dados em um buffer de 1024 bytes
-                DatagramPacket receiveBuffer = new DatagramPacket(new byte[1024], 1024, address, port);
-                socket.receive(receiveBuffer);//recepção
-                
-                //imprime a msg recebida
-                String receiveMessage = new String(receiveBuffer.getData());
-                System.out.println("O servidor disse: " + receiveMessage);
-            } 
-            catch (IOException ex) 
-            {
-                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                System.err.println(ex);
             }
         }
     }
-//    public static void main(String[] args) {
-//        int port = 12345;
-//        DatagramSocket socket = null;
-//        try {
-//            InetAddress address = InetAddress.getByName("127.0.0.1"); //broadcast "255.255.255.255"
-//            socket = new DatagramSocket();
-//            while (true) {
-//                //envio de dados para o emissor do datagrama recebido
-//                String message = JOptionPane.showInputDialog(null, "Digite a mensagem");
-//                byte[] data = message.getBytes();
-//                DatagramPacket sendBuffer = new DatagramPacket(data, data.length, address, port);
-//                socket.send(sendBuffer);//envio
-//                //encerra a conexao
-//                if (message.equalsIgnoreCase("sair")) {
-//                    socket.close();
-//                }
-//                //recebimento dos dados em um buffer de 1024 bytes
-//                DatagramPacket receiveBuffer = new DatagramPacket(new byte[1024], 1024, address, port);
-//                socket.receive(receiveBuffer);//recepção
-//                //imprime a msg recebida
-//                String receiveMessage = new String(receiveBuffer.getData());
-//                System.out.println("O servidor disse: " + receiveMessage);
-//            }
-//        } catch (Exception e) {
-//            System.out.println(e);
-//            if (socket != null) {
-//                socket.close();
-//            }
-//        }
-//    }
 }
